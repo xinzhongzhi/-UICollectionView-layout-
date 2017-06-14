61aac21389a5b26e8e1cc8e1695f4d402dfaedcb
risenb_xinzhongzhi <xinzz_sy_risenb@163.com>
2017-06-14 10:36:49 +0800

    Initial Commit

---
+++ 自定义CollectionViewLayout/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  自定义CollectionViewLayout
+//
+//  Created by 辛忠志 on 2017/6/14.
+//  Copyright © 2017年 辛忠志. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 自定义CollectionViewLayout/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  自定义CollectionViewLayout
+//
+//  Created by 辛忠志 on 2017/6/14.
+//  Copyright © 2017年 辛忠志. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
